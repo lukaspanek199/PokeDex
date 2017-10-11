42a6bea2a945a0a1e4b7a5bece6b665f9d73d4fe
Jan Sauer <jan.sauer956@gmail.com>
2017-10-11 09:55:52 +0200

    Edited Profil.Class.php

--- Profil.Class.php
+++ Profil.Class.php
@@ -4,12 +4,13 @@
    */
   class Profil
   {
-    $servername="mysql.spse-net.cz";
-    $username="paneklu";
-    $pass="tygr348";
-    $registername="";
-    $regpass="";
-    $dbname="paneklu";
+    private $srvname="mysql.spse-net.cz";
+    private $username="paneklu";
+    private $pass="tygr348";
+    private $registername="";
+    private $regpass="";
+    private $dbname="paneklu";
+
 
 
     function __construct($registername, $regpass)
@@ -21,14 +22,14 @@
     public function Register(){
 
 
-      $conn =new mysqli($servername,$username,$pass,$dbname);
+      $conn =new mysqli($this->srvname,$this->username,$this->pass,$this->dbname);
       if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
       }
       echo "Connected successfully";
 
-      //$hashed=hash("ripemd5",$this->regpass);
-      $sql="INSERT INTO profily (registername,pass) VALUES ($this->registername,$this->regpass)";
+      $hashed=hash("md5",$this->regpass);
+      $sql="INSERT INTO profily (registername,pass) VALUES ('$this->registername','$hashed')";
 
       if ($conn->query($sql) === TRUE) {
         echo "New record created successfully";
