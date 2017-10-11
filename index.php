63fd3a654de689cfc0ebdd91759eb0b45470fb5f
Lukáš Pánek <lukaspanek199@users.noreply.github.com>
2017-10-11 10:10:00 +0200

    Finished implementation of NewPokemon function

--- index.php
+++ index.php
@@ -6,4 +6,5 @@
   $profil->Register();
 
   $pokemon = new Pokemon("Pikachu", "Pikachu (Japanese: ピカチュウ Pikachu) is an Electric-type Pokémon introduced in Generation I.","https://cdn.bulbagarden.net/upload/0/0d/025Pikachu.png");
+  $pokemon->NewPokemon();
 ?>
